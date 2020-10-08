820fefcfd2119a3b1e801265885ea8678350b2c2
Sascha L. Teichmann <sascha.teichmann@intevation.de>
2020-10-08 12:51:48 +0200

    Fixed sql stub for extension.

--- ical.in.sql
+++ ical.in.sql
@@ -1,4 +1,9 @@
-CREATE FUNCTION sql_next_time_ical(ical text, tz text, p_offset text)
-    RETURNS Datum
+CREATE OR REPLACE FUNCTION next_time_ical (text, text)
+    RETURNS integer
+    LANGUAGE C STRICT
+    AS 'MODULE_PATHNAME', $$sql_next_time_ical$$;
+
+CREATE OR REPLACE FUNCTION next_time_ical (text, text, integer)
+    RETURNS integer
     LANGUAGE C STRICT
     AS 'MODULE_PATHNAME', $$sql_next_time_ical$$;
