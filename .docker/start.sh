7172794c287d16fcda7909634244d847088d0c21
Björn Ricks <bjoern.ricks@greenbone.net>
2021-11-15 17:16:51 +0100

    Add a start script for setting up postgres
    When starting the pg-gvm container a postgres server should be set up
    and started.

---
+++ .docker/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+[ -z "$GVMD_USER" ] && GVMD_USER="gvmd"
+
+pg_ctlcluster 13 main start
+
+createuser -DRS "$GVMD_USER"
+createdb -O gvmd "$GVMD_USER"
+
+psql -d gvmd -c "create role dba with superuser noinherit;"
+psql -d gvmd -c "grant dba to $GVMD_USER;"
+psql -d gvmd -c 'create extension "uuid-ossp";'
+psql -d gvmd -c 'create extension "pgcrypto";'
+psql -d gvmd -c 'create extension "pg-gvm";'
+
+pg_ctlcluster --foreground 13 main stop
+pg_ctlcluster --foreground 13 main start
