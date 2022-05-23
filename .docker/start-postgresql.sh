81ca903a2f414161cf639ec68ec34bd06cdae9dc
Björn Ricks <bjoern.ricks@greenbone.net>
2022-05-23 16:47:18 +0200

    Setup postgres on non accessible unix socket
    Run the setup of postgres on a socket in the /tmp directory which should
    be private for the container.

--- .docker/start-postgresql.sh
+++ .docker/start-postgresql.sh
@@ -5,16 +5,16 @@
 
 rm -f $PGRES_DATA/started
 
-pg_ctlcluster 13 main start
+pg_ctlcluster -o "-k /tmp" 13 main start
 
-createuser -DRS "$GVMD_USER"
-createdb -O gvmd "$GVMD_USER"
+createuser --host=/tmp -DRS "$GVMD_USER"
+createdb --host=/tmp -O gvmd "$GVMD_USER"
 
-psql -d gvmd -c "create role dba with superuser noinherit;"
-psql -d gvmd -c "grant dba to $GVMD_USER;"
-psql -d gvmd -c 'create extension "uuid-ossp";'
-psql -d gvmd -c 'create extension "pgcrypto";'
-psql -d gvmd -c 'create extension "pg-gvm";'
+psql --host=/tmp -d gvmd -c "create role dba with superuser noinherit;"
+psql --host=/tmp -d gvmd -c "grant dba to $GVMD_USER;"
+psql --host=/tmp -d gvmd -c 'create extension "uuid-ossp";'
+psql --host=/tmp -d gvmd -c 'create extension "pgcrypto";'
+psql --host=/tmp -d gvmd -c 'create extension "pg-gvm";'
 
 pg_ctlcluster --foreground 13 main stop
 # Touch file, signaling startup is done
