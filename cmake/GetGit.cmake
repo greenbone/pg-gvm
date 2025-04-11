6e4c423af9cdcb142571447de93c94d5cddc0f5e
Björn Ricks <bjoern.ricks@greenbone.net>
2025-04-11 12:02:58 +0200

    misc: Use lower case name for CMake macro get_git_revision
    gersemi uses the exact name during formatting.

--- cmake/GetGit.cmake
+++ cmake/GetGit.cmake
@@ -21,7 +21,7 @@
 
 find_package (Git)
 
-macro (Git_GET_REVISION dir variable)
+macro (git_get_revision dir variable)
   execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
                   WORKING_DIRECTORY ${dir}
                   OUTPUT_VARIABLE GIT_BRANCH
@@ -32,7 +32,7 @@ macro (Git_GET_REVISION dir variable)
                   OUTPUT_STRIP_TRAILING_WHITESPACE)
   string (REPLACE "/" "_" GIT_BRANCH ${GIT_BRANCH})
   set (${variable} "${GIT_COMMIT_HASH}-${GIT_BRANCH}")
-endmacro (Git_GET_REVISION)
+endmacro ()
 
 if (EXISTS "${SOURCE_DIR}/.git/")
   if (GIT_EXECUTABLE)
